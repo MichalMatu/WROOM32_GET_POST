21ae3c34e759c57c7e777fbd7a4019851da85a3a
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-05-29 23:21:36 +0200

    eureka

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,27 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <SPIFFS.h>
+// declare onboard LED pin
+#define LED 2
 
 const char *ssid = "ESP32-AP";
 const char *password = "password";
 WebServer server(80);
 
-String value = "Initial Value"; // The value to be updated
+int value = 1000; // The value to be updated
+unsigned long previousMillis = 0;
+unsigned long interval = 1000; // Initial interval for LED blinking (in milliseconds)
+bool ledState = false;         // LED state
 
 void handleRoot()
 {
   File file = SPIFFS.open("/index.html", "r");
   if (file)
   {
-    server.streamFile(file, "text/html");
+    String html = file.readString();
     file.close();
+    html.replace("{value_placeholder}", String(value));
+    server.send(200, "text/html", html);
   }
   else
   {
@@ -26,8 +33,8 @@ void handleUpdate()
 {
   if (server.hasArg("value"))
   {
-    value = server.arg("value");
-    server.send(200, "text/plain", value);
+    value = server.arg("value").toInt();
+    server.send(200, "text/plain", String(value));
   }
   else
   {
@@ -35,9 +42,15 @@ void handleUpdate()
   }
 }
 
+void handleGetValue()
+{
+  server.send(200, "text/plain", String(value));
+}
+
 void setup()
 {
   Serial.begin(115200);
+  pinMode(LED, OUTPUT);
 
   WiFi.softAP(ssid, password);
   IPAddress IP = WiFi.softAPIP();
@@ -52,6 +65,7 @@ void setup()
 
   server.on("/", handleRoot);
   server.on("/update", handleUpdate);
+  server.on("/getValue", handleGetValue);
 
   server.begin();
 }
@@ -59,4 +73,14 @@ void setup()
 void loop()
 {
   server.handleClient();
+
+    unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= value)
+  {
+    previousMillis = currentMillis;
+    // print in console the current int value and its type
+    Serial.print("value: ");
+    Serial.print(value);
+    Serial.print(" type: ");
+  }
 }
