06bbda027ee8c7975d483153c73a0d41749655b7
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-06-03 02:45:54 +0200

    clean up

--- data/script.js
+++ data/script.js
@@ -1,22 +1,36 @@
-// Access the DOM elements
-const valueDisplay = document.getElementById("sensor-value");
-
-console.log("Connecting to WebSocket server");
-
-// Create a WebSocket connection
-const socket = new WebSocket("ws://" + window.location.hostname + ":81/");
-
-// Handle received WebSocket messages
-socket.onmessage = function (event) {
-  console.log("Received message:", event.data);
-  const sensorValue = event.data;
-  valueDisplay.innerText = sensorValue;
-};
-
-socket.onopen = function (event) {
-  console.log("WebSocket connection opened");
-};
-
-socket.onerror = function (event) {
-  console.error("WebSocket error observed:", event);
-};
+var gateway = `ws://${window.location.hostname}/ws`;
+var websocket;
+window.addEventListener("load", onLoad);
+function initWebSocket() {
+  console.log("Trying to open a WebSocket connection...");
+  websocket = new WebSocket(gateway);
+  websocket.onopen = onOpen;
+  websocket.onclose = onClose;
+  websocket.onmessage = onMessage; // <-- add this line
+}
+function onOpen(event) {
+  console.log("Connection opened");
+}
+function onClose(event) {
+  console.log("Connection closed");
+  setTimeout(initWebSocket, 2000);
+}
+function onMessage(event) {
+  var state;
+  if (event.data == "1") {
+    state = "ON";
+  } else {
+    state = "OFF";
+  }
+  document.getElementById("state").innerHTML = state;
+}
+function onLoad(event) {
+  initWebSocket();
+  initButton();
+}
+function initButton() {
+  document.getElementById("button").addEventListener("click", toggle);
+}
+function toggle() {
+  websocket.send("toggle");
+}
